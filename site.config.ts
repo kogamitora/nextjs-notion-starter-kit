d1d7a219c6e2604f4c825d1c199a459acf8a242b
wenshuai <1356561149@qq.com>
2023-02-15 15:46:31 +0800

    update basic info

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Kogami Blog',
+  domain: 'https://kogamitora.monster',
+  author: 'KogamiTora',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Kogami Blog',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
